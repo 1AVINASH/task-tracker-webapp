eb828288c28f0b1747b82548e1eb88b86e3aacad
avinash <avinash24061998@gmail.com>
2025-07-03 17:14:05 +0530

    Fixing setup file to run frontend in the foreground but backend in background

--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@ cd infra
 docker compose up -d
 cd ..
 cd backend
-pipenv shell
 lsof -i :8000 | tail -n +2 | awk '{print $2}' | xargs -r kill -9
-nohup python3 app.py &
+pipenv run nohup python3 app.py &
 cd ..
 cd frontend
 lsof -i :3000 | tail -n +2 | awk '{print $2}' | xargs -r kill -9
-nohup npm run start &
-cd ..
+npm run start
